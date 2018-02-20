66a2c5a03e5f4796979babf90b0fbcc1a7872092
Mateu Aguiló Bosch <mateu@lullabot.com>
2018-02-21 00:23:30 +0100

    feat(performance): run middlewares directly (#29)
    BREAKING CHANGE: you need to pass the express app variable when initializing the subrequest route.

---
+++ src/executeRoute.js
@@ -0,0 +1,120 @@
+const _ = require('lodash');
+
+/**
+ * Creates a new request object based on an existing request and some overrides.
+ *
+ * @param {string} path
+ *   The route path to execute.
+ * @param {IncomingMessage} originalReq
+ *   The original request object.
+ * @param {Object} requestOverrides
+ *   Manual overrides for the request.
+ *
+ * @return {Object}
+ *   The newly created request object.
+ */
+const createRequest = (path, originalReq, requestOverrides = {}) => {
+  // Clone the original request.
+  const newReq = _.cloneDeep(originalReq);
+  // Delete all private properties since those should be computed.
+  Object.keys(newReq)
+    .filter(key => key[0] === '_')
+    .forEach((key) => {
+      delete newReq[key];
+    });
+  // Apply overrides.
+  Object.keys(requestOverrides)
+    .forEach((key) => {
+      newReq[key] = requestOverrides[key];
+    });
+  // Override the URL.
+  newReq.url = path;
+  newReq.originalUrl = path;
+  // Make sure the request method is uppercase.
+  newReq.method = newReq.method.toUpperCase();
+  newReq.originalMethod = newReq.method;
+
+  return newReq;
+};
+
+/**
+ * Creates a response that other middlewares can interact with.
+ *
+ * @param {ServerResponse} originalRes
+ *   The original response.
+ * @param {IncomingMessage} req
+ *   The modified request object.
+ * @param {function} callback
+ *   Instead of terminating the response as express would normally do upon
+ *   calling res.end, we will execute this callback.
+ *
+ * @return {Object}
+ *   A response mock that contains the most commonly called response methods.
+ */
+const createResponse = (originalRes, req, callback) => {
+  // Clone the original response.
+  const newRes = _.cloneDeep(originalRes);
+  // Set the modified request object.
+  newRes.req = req;
+
+  // Response termination using our custom callback instead.
+  newRes.end = (data) => {
+    if (callback) {
+      const headers = Object.keys(newRes._headerNames).reduce(
+        (heads, name) => Object.assign(heads, { [name]: newRes.getHeader(name) }),
+        {}
+      );
+      callback(data, newRes.statusCode, headers);
+    }
+  };
+  newRes.send = newRes.end;
+  newRes.write = newRes.end;
+
+  return newRes;
+};
+
+/**
+ * Creates the request and response objects.
+ *
+ * @param {string} path
+ *   The route path to execute.
+ * @param {IncomingMessage} req
+ *   The original request object.
+ * @param {ServerResponse} res
+ *   The original response object.
+ * @param {Object} requestOverrides
+ *   Manual overrides for the request.
+ * @param {function} callback
+ *   Instead of terminating the response as express would normally do upon
+ *   calling res.end, we will execute this callback.
+ *
+ * @return {[IncomingMessage, ServerResponse]}
+ *   A tuple containing the request and response objects.
+ */
+const createRequestAndResponse = (path, req, res, requestOverrides, callback) => {
+  if (callback) {
+    callback = _.once(callback);
+  }
+  const newRew = createRequest(path, req, requestOverrides);
+  const newRes = createResponse(res, newRew, callback);
+  return [newRew, newRes];
+};
+
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    req.runMiddleware = (path, requestOverrides, callback) => {
+      app.runMiddleware(path, req, res, requestOverrides, callback);
+    };
+    next();
+  });
+
+  // Only add once to the app object.
+  if (app.runMiddleware) {
+    return;
+  }
+
+  app.runMiddleware = (path, req, res, requestOverrides, callback) => {
+    const tuple = createRequestAndResponse(path, req, res, requestOverrides, callback);
+    app(tuple[0], tuple[1]);
+  };
+};
--- test/ExpressRequestorTest.js
+++ test/ExpressRequestorTest.js
@@ -1,19 +1,30 @@
 const ExpressRequestor = require('../lib/ExpressRequestor');
+const qs = require('qs');
 const sinon = require('sinon');
 
 module.exports = {
   testConstructor(test) {
     test.expect(2);
-    const sut = new ExpressRequestor({ protocol: 'foo', headers: { host: 'bar' } });
+    const sut = new ExpressRequestor({
+      protocol: 'foo',
+      headers: { host: 'bar' },
+      runMiddleware: sinon.stub(),
+    });
     test.equal(sut.host, 'bar');
     test.equal(sut.protocol, 'foo');
     test.done();
   },
   test_individualRequest(test) {
     test.expect(8);
-    const sut = new ExpressRequestor({ protocol: 'foo', headers: { host: 'bar' } });
+    const sut = new ExpressRequestor({
+      protocol: 'foo',
+      headers: { host: 'bar' },
+      runMiddleware: sinon.stub(),
+    });
     sut._doRequest = sinon.stub();
     sut._doRequest.returns(null);
+    sut._doExternalRequest = sinon.stub();
+    sut._doExternalRequest.returns(null);
     sut._individualRequest(
       'oof',
       '/lorem/ipsum',
@@ -23,24 +34,49 @@ module.exports = {
     sut._individualRequest(
       'rab',
       'https://lorem/ipsum',
-      { a: 'b' },
+      { headers: { a: 'b' }, body: 'the-body' },
       'fakeReq2'
     );
     const args1 = sut._doRequest.args[0];
-    const args2 = sut._doRequest.args[1];
+    const args2 = sut._doExternalRequest.args[0];
     test.equal(args1[0], 'oof');
-    test.equal(args1[1], 'foo://bar/lorem/ipsum');
-    test.deepEqual(args1[2], {});
+    test.equal(args1[1], '/lorem/ipsum');
+    test.deepEqual(args1[2], {
+      query: {},
+      method: 'oof',
+      headers: {},
+      rawHeaders: [],
+    });
     test.equal(args1[3], 'fakeReq');
     test.equal(args2[0], 'rab');
     test.equal(args2[1], 'https://lorem/ipsum');
-    test.deepEqual(args2[2], { a: 'b' });
+    test.deepEqual(args2[2], { headers: { a: 'b' }, body: 'the-body' });
     test.equal(args2[3], 'fakeReq2');
     test.done();
   },
+  test_doExternalRequest(test) {
+    test.expect(1);
+    const sut = new ExpressRequestor({
+      protocol: 'foo',
+      headers: { host: 'bar' },
+      runMiddleware: sinon.stub(),
+    });
+    const res = sut._doExternalRequest(
+      'oof',
+      'https://lorem/ipsum',
+      {},
+      'fakeReq'
+    );
+    test.ok(res instanceof Promise);
+    res.catch(() => test.done());
+  },
   test_doRequest(test) {
     test.expect(1);
-    const sut = new ExpressRequestor({ protocol: 'foo', headers: { host: 'bar' } });
+    const sut = new ExpressRequestor({
+      protocol: 'foo',
+      headers: { host: 'bar' },
+      runMiddleware: sinon.stub(),
+    });
     const res = sut._doRequest(
       'oof',
       '/lorem/ipsum',
@@ -48,6 +84,51 @@ module.exports = {
       'fakeReq'
     );
     test.ok(res instanceof Promise);
-    res.catch(() => test.done());
+    test.done();
+  },
+  testRunMiddleware(test) {
+    test.expect(1);
+    const sut = new ExpressRequestor({
+      protocol: 'foo',
+      headers: { host: 'bar' },
+      runMiddleware: (uri, requestOverrides, cb) => {
+        cb({ the: 'body' }, 999, { kii: 'eef' });
+      },
+    });
+    sut._doRequest(
+      'oof',
+      '/lorem/ipsum',
+      {},
+      'fakeReq'
+    )
+      .then((res) => {
+        test.deepEqual(res, {
+          body: '{"the":"body"}',
+          headers: { kii: 'eef', 'x-subrequest-id': 'fakeReq' },
+        });
+        test.done();
+      });
+  },
+  testMissingRunMiddleware(test) {
+    test.expect(1);
+    test.throws(() => new ExpressRequestor({
+      protocol: 'foo',
+      headers: { host: 'bar' },
+    }), 'Error');
+    test.done();
+  },
+  test_parseQueryString(test) {
+    test.expect(2);
+    const sut = new ExpressRequestor({
+      protocol: 'foo',
+      headers: { host: 'bar' },
+      runMiddleware: sinon.stub(),
+    });
+    sinon.stub(qs, 'parse');
+    const res = sut._parseQueryString('foo#bar$baz');
+    test.equal(res.uri, 'foo#bar$baz');
+    test.ok(qs.parse.called);
+    qs.parse.restore();
+    test.done();
   },
 };
