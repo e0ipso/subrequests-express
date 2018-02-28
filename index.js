0ccd3e47a37dd3522ad72798655c35bbeabffdc3
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2018-02-28 15:56:21 +0100

    feat(requestor): allow injecting requestor object

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // The ./lib directory is generated at installation time by the flow
 // compilation process.
 const subrequestsRouterFactory = require('./lib');
+const ExpressRequestor = require('./lib/ExpressRequestor');
 
-module.exports = { subrequestsRouterFactory };
+module.exports = { subrequestsRouterFactory, ExpressRequestor };
